e92bd13176129b63d62c8b1d5f37ac6a83274732
tumbling-dice <inujini.dev@gmail.com>
2014-08-12 18:15:55 +0900

    疲労困憊

--- ImageViewer/HttpImageManager.java
+++ ImageViewer/HttpImageManager.java
@@ -5,12 +5,63 @@ public final class HttpImageManager {
 	private static final int MSG_ONERROR = 1;
 	
 	private static ThreadPoolExecutor _executor;
+	/** 処理中のkeyを保存しておくList */
+	private static volatile ArrayList<Object> _keyHolder = new ArrayList<Object>();
 	
 	static {
-		int coreCount = Runtime.getRuntime().availableProcessors();
+		_executor = init();
+	}
+	
+	public static void execute(Object key, ImageCaller caller, ImageCallback callback, boolean isThrowException) {
+		if(_keyHolder == null) {
+			_keyHolder = new ArrayList<Object>();
+		} else if(_keyHolder.contains(key)) {
+			return;
+		}
 		
-		_executor = new ThreadPoolExecutor(coreCount, MAX_THREAD, 1L, TimeUnit.SECOND
-			, new LinkedBlockingQueue<ImageRunnable>(coreCount)
+		_keyHolder.add(key);
+		
+		if(_executor == null) _executor = init();
+		_executor.execute(new ImageFuture(key, caller, callback, isThrowException));
+	}
+	
+	public static void execute(Object key, ImageCaller caller, ImageCallback callback, ErrorHandler onError) {
+		if(_keyHolder == null) {
+			_keyHolder = new ArrayList<Object>();
+		} else if(_keyHolder.contains(key)) {
+			return;
+		}
+		
+		_keyHolder.add(key);
+		
+		if(_executor == null) _executor = init();
+		_executor.execute(new ImageFuture(key, caller, callback, onError));
+	}
+	
+	public static void shutdown() {
+		
+		if(_executor == null) return;
+		
+		try {
+			_executor.shutdown();
+			if(!_executor.awaitTermination(1L, TimeUnit.SECOND)) {
+				_executor.shutdownNow();
+			}
+		} catch(InterruptedException e) {
+			_executor.shutdownNow();
+		} finally {
+			_executor = null;
+			if(_keyHolder != null) {
+				_keyHolder.clear();
+				_keyHolder = null;
+			}
+		}
+	}
+	
+	private static ThreadPoolExecutor init() {
+		return new ThreadPoolExecutor(3, MAX_THREAD
+			, 1L, TimeUnit.SECOND
+			, new LinkedBlockingQueue<Runnable>(10)
 			, new ThreadFactory() {
 				private final AtomicInteger _count = new AtomicInteger(1);
 				
@@ -29,14 +80,17 @@ public final class HttpImageManager {
 			});
 	}
 	
+	/** Handler内で使用するコールバック */
 	public interface ImageCallback {
 		void call(Object key, Bitmap bitmap);
 	}
 	
+	/** Handler内で使用するエラーハンドラ */
 	public interface ErrorHandler {
 		void call(Exception e);
 	}
 	
+	/** バックグラウンドスレッドで実行する内容（Caller） */
 	public static class ImageCaller implements Callable<BitmapResult> {
 		
 		private final String _url
@@ -64,77 +118,31 @@ public final class HttpImageManager {
 		
 		@Override
 		public BitmapResult call() throws Exception {
-			Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+			android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
 			return ImageUtil.getBitmapFromHttp(_url, _maxSize, _timeout, _isSaveCache, _isSaveFile);
 		}
 	}
 	
-	static class ImageHandler extends Handler {
-		
-		private Object _key;
-		private ImageCallback _callback;
-		private ErrorHandler _onError;
-		private boolean _isThrowException;
-		
-		public ImageHandler(Looper looper, Object key, ImageCallback callback, boolean isThrowException) {
-			super(looper);
-			_key = key;
-			_callback = callback;
-			_isThrowException = isThrowException;
-		}
-		
-		public ImageHandler(Looper looper, Object key, ImageCallback callback, ErrorHandler onError) {
-			super(looper);
-			_key = key;
-			_callback = callback;
-			_onError = onError;
-		}
-		
-		public void setNotThrowException() {
-			_isThrowException = false;
-		}
-		
-		@Override
-		public void handleMessage(Message msg) {
-			switch (msg.what) {
-			case MSG_CALLBACK:
-				_callback.call(_key, (Bitmap) msg.obj);
-				break;
-			case MSG_ONERROR:
-				Exception e = (Exception) msg.obj;
-				if(_onError != null) {
-					_onError.call(e);
-				} else if(_isThrowException) {
-					_callback = null;
-					new RuntimeException(e);
-				} else {
-					e.printStackTrace();
-				}
-				break;
-			}
-			
-			_callback = null;
-			_onError = null;
-			_key = null;
-		}
-	}
-	
+	/** ImageCallerの処理結果を受け取り、ImageHandlerに渡すFutureTask */
 	static class ImageFuture extends FutureTask<BitmapResult> {
 		
 		private ImageHandler _handler;
 		
-		public ImageFuture(Looper looper, Object key, ImageCaller caller, ImageCallback callback, boolean isThrowException) {
+		public ImageFuture(Object key, ImageCaller caller, ImageCallback callback, boolean isThrowException) {
 			super(caller);
-			_handler = new ImageHandler(looper, key, callback, isThrowException);
+			_handler = new ImageHandler(key, callback, isThrowException);
 		}
 		
-		public ImageFuture(Looper looper, Object key, ImageCaller caller, ImageCallback callback, ErrorHandler onError) {
+		public ImageFuture(Object key, ImageCaller caller, ImageCallback callback, ErrorHandler onError) {
 			super(caller);
-			_handler = new ImageHandler(looper, key, callback, onError);
+			_handler = new ImageHandler(key, callback, onError);
 		}
 		
 		@Override
 		protected void done() {
+			// 例外が発生してなければMessage#whatにMSG_CALLBACKを、objにbitmapを渡す
+			// （Executor関連も含む）例外が発生した場合はMessage#whatにMSG_ONERRORを、objにExceptionを渡す
+			
 			try {
 				BitmapResult result = get();
 				
@@ -160,22 +168,62 @@ public final class HttpImageManager {
 		}
 	}
 	
-	public static void execute(Object key, ImageCaller caller, ImageCallback callback, boolean isThrowException) {
-		_executor.execute(new ImageFuture(Looper.getMainLooper(), key, caller, callback, isThrowException));
-	}
-	
-	public static void execute(Object key, ImageCaller caller, ImageCallback callback, ErrorHandler onError) {
-		_executor.execute(new ImageFuture(Looper.getMainLooper(), key, caller, callback, onError));
-	}
-	
-	public static void shutdown() {
-		try {
-			_executor.shutdown();
-			if(!_executor.awaitTermination(1L, TimeUnit.SECOND)) {
-				_executor.shutdownNow();
+	/** UIスレッド内で実行する内容（Handler） */
+	static class ImageHandler extends Handler {
+		
+		private Object _key;
+		private ImageCallback _callback;
+		private ErrorHandler _onError;
+		private boolean _isThrowException;
+		
+		// コンストラクタはHandler(Looper)を継承する
+		// UIスレッドで確実に実行するため、LooperはLooper#getMainLooper()のみを使用する
+		
+		public ImageHandler(Object key, ImageCallback callback, boolean isThrowException) {
+			super(Looper.getMainLooper());
+			_key = key;
+			_callback = callback;
+			_isThrowException = isThrowException;
+		}
+		
+		public ImageHandler(Object key, ImageCallback callback, ErrorHandler onError) {
+			super(Looper.getMainLooper());
+			_key = key;
+			_callback = callback;
+			_onError = onError;
+		}
+		
+		public void setNotThrowException() {
+			_isThrowException = false;
+		}
+		
+		@Override
+		public void handleMessage(Message msg) {
+			if(_keyHolder != null) _keyHolder.remove(_key);
+			
+			switch (msg.what) {
+			case MSG_CALLBACK:
+				_callback.call(_key, (Bitmap) msg.obj);
+				break;
+			case MSG_ONERROR:
+				Exception e = (Exception) msg.obj;
+				if(_onError != null) {
+					_onError.call(e);
+				} else if(_isThrowException) {
+					_callback = null;
+					_onError = null;
+					_key = null;
+					new RuntimeException(e);
+				} else {
+					e.printStackTrace();
+				}
+				break;
 			}
-		} catch(InterruptedException e) {
-			_executor.shutdownNow();
+			
+			_callback = null;
+			_onError = null;
+			_key = null;
 		}
 	}
+	
 }
