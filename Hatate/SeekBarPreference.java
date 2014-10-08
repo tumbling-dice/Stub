456bfe640bf074a76d3687ef1d83c0513ba89247
tumbling-dice <inujini.dev@gmail.com>
2014-10-08 18:30:19 +0900

    brush up

--- Hatate/SeekBarPreference.java
+++ Hatate/SeekBarPreference.java
@@ -2,15 +2,25 @@ public class SeekBarPreference extends Preference implements OnSeekBarChangeList
 
 	@Accessors(prefix="_") @Getter private int _max;
 	@Accessors(prefix="_") @Getter private int _currentValue;
-	
+
 	private WeakReference<TextView> _txvValue;
 	private WeakReference<SeekBar> _seekBar;
+
+	private static final class SeekBarPreferenceViewHolder {
+		public final TextView txtValue;
+		public final SeekBar seekBar;
+
+		public SeekBarPreferenceViewHolder(View view) {
+			txtValue = (TextView) view.findViewById(R.id.txvValue);
+			seekBar = (SeekBar) view.findViewById(R.id.skbVolume);
+		}
+	}
 
 	public SeekBarPreference(Context context, int max) {
 		super(context);
 		_max = max;
 	}
-	
+
 	public SeekBarPreference(Context context, int max, int currentValue) {
 		super(context);
 		_max = max;
@@ -26,17 +36,17 @@ public class SeekBarPreference extends Preference implements OnSeekBarChangeList
 		super(context, attrs, defStyle);
 		init(context, attrs);
 	}
-	
+
 	private void init(Context context, AttributeSet attrs) {
-		
+
 		// SeekBarPreference's attrs + android Preference's attrs
 		val attrIds = new int[R.styleable.SeekBarPreference.length + android.R.styleable.Preference]
 		System.arraycopy(R.styleable.SeekBarPreference, 0, attrIds, 0, R.styleable.SeekBarPreference.length);
 		System.arraycopy(android.R.styleable.Preference, 0, attrIds
 			, R.styleable.SeekBarPreference.length, android.R.styleable.Preference.length);
-		
+
 		@Cleanup("recycle") val t = context.obtainStyledAttributes(attrs, attrIds);
-		
+
 		// get max value
 		_max = t.getInt(R.styleable.VolumePreference_max, 0);
 		
@@ -45,9 +55,9 @@ public class SeekBarPreference extends Preference implements OnSeekBarChangeList
 		_currentValue = _currentValue != -1
 							? _currentValue
 							: t.getInt(android.R.styleable.Preference_defaultValue, 0);
-		
+
 	}
-	
+
 	@Override
 	protected View onCreateView(ViewGroup parent) {
 		setWidgetLayoutResource(R.layout.seekbar_preference);
@@ -57,26 +67,35 @@ public class SeekBarPreference extends Preference implements OnSeekBarChangeList
 	@Override
 	protected void onBindView(View view) {
 		super.onBindView(view);
-		
-		val txtValue = (TextView) view.findViewById(R.id.txvValue);
-		txtValue.setText(String.format("%d/%d", _currentValue, _max));
-		if(_txvValue != null) _txvValue.clear();
-		_txvValue = new WeakReference<TextView>(txtValue);
 
-		val seekBar = (SeekBar) view.findViewById(R.id.skbVolume);
-		seekBar.setMax(_max);
-		seekBar.setProgress(_currentValue);
-		seekBar.setOnSeekBarChangeListener(this);
+		val tag = view.getTag();
+		SeekBarPreferenceViewHolder vh = null;
+
+		if(tag != null && tag instanceof SeekBarPreferenceViewHolder) {
+			vh = (SeekBarPreferenceViewHolder) tag;
+		} else {
+			vh = new SeekBarPreferenceViewHolder(view);
+			view.setTag(vh);
+			vh.seekBar.setOnSeekBarChangeListener(this);
+		}
+
+		vh.txtValue.setText(String.format("%d/%d", _currentValue, _max));
+		if(_txvValue != null) _txvValue.clear();
+		_txvValue = new WeakReference<TextView>(vh.txtValue);
+
+		vh.seekBar.setMax(_max);
+		vh.seekBar.setProgress(_currentValue);
+
 		if(_seekBar != null) _seekBar.clear();
-		_seekBar = new WeakReference<SeekBar>(seekBar);
+		_seekBar = new WeakReference<SeekBar>(vh.seekBar);
 	}
-	
+
 	public void setMax(int max) {
 		_max = max;
 		changeTextView();
-		
+
 		if(_seekBar == null) return;
-		
+
 		val seekBar = _seekBar.get();
 		if(seekBar != null) seekBar.setMax(max);
 	}
@@ -84,16 +103,16 @@ public class SeekBarPreference extends Preference implements OnSeekBarChangeList
 	public void setCurrentValue(int currentValue) {
 		_currentValue = currentValue;
 		changeTextView();
-		
+
 		if(_seekBar == null) return;
-		
+
 		val seekBar = _seekBar.get();
 		if(seekBar != null) seekBar.setProgress(currentValue);
 	}
 	
 	private void changeTextView() {
 		if(_txvValue == null) return;
-		
+
 		val txtView = _txvValue.get();
 		if(txtView != null) txtView.setText(String.format("%d/%d", _currentValue, _max));
 	}
@@ -110,10 +129,10 @@ public class SeekBarPreference extends Preference implements OnSeekBarChangeList
 	@Override
 	public void onStopTrackingTouch(SeekBar seekBar) {
 		_currentValue = seekBar.getProgress();
-		
+
 		// save value
 		super.getEditor().putInt(super.getKey(), _currentValue).commit();
-		
+
 		val changedListener = super.getOnPreferenceChangeListener();
 		if(changedListener != null)
 			changedListener.onPreferenceChange(this, _currentValue);
